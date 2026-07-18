b63ca3b4c9934b3fbf7df1f51a8584d0cd9b6ea0
prahlad122 <prahladji1592003@gmail.com>
2026-07-18 07:18:19 +0530

    fixed cors

--- municipal-backend/src/app.js
+++ municipal-backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-  origin:  process.env.FRONTEND_URL,
+  origin:  "https://smartcomplaints.vercel.app",
   credentials: true
 }));
 app.use(express.json());
